34936331c71c609e2876e919303cd86413a929ff
Ade Oshineye <adewale@gmail.com>
2011-04-06 12:19:34 +0100

    Acceptance test 9 passes

--- code/acceptance-tests/AcceptanceTest.java
+++ code/acceptance-tests/AcceptanceTest.java
@@ -35,7 +35,7 @@ public  class AcceptanceTest {
 			testData.add(new String[]{testName});
 		}
 		//TODO(ade) Restrict to first N acceptance tests so that I can see what's going on
-		return testData.subList(0, 7);
+		return testData.subList(0, 9);
 	}
 	
 	@Test
